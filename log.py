cb7f9303035dc638c640932a4a8a23eb4acdc123
bommusrinivasu <38848779+bommusrinivasu@users.noreply.github.com>
2018-06-12 09:39:44 +0530

    Update log.py

--- log.py
+++ log.py
@@ -2,6 +2,7 @@
 
 import psycopg2
 
+
 # Here goal receives the query
 def execute(goal):
     """ This function connects to the database,
@@ -17,26 +18,29 @@ def execute(goal):
         return None
     finally:
         return data
-    
+
 
 """This function returns the top three popular reading articles"""
+
+
 def articles():
     print('\nTop three articles of all time by views:')
-    output = execute("""  
+    output = execute("""
         select title,count(*) as num from articles,log where
         log.path=CONCAT('/article/',articles.slug) group by
         articles.title order by
-        num DESC limit 3; 
-    """
-    )
+        num DESC limit 3;
+                 """)
     for result in output:
-               print ("%s: %s views" % (result[0],result[1]))
+                print ("%s: %s views" % (result[0], result[1]))
 
 """This function returns the top three most popular authors"""
+
+
 def authors():
 
     # this calling function passes the query
-    output = execute( """
+    output = execute("""
        SELECT authors.name,count(*) FROM log,articles,authors
        WHERE  log.path = '/article/' || articles.slug
        AND articles.author = authors.id
@@ -50,20 +54,22 @@ def authors():
                 print ("%s: %s views" % (r[0], r[1]))
 
 """This function returns days with more than 1% errors"""
+
+
 def errors():
-    
+
     print('\nDays with more than 1% error:')
-    
+
     # This calling function passes the query
-    output = execute( """
+    output = execute("""
     select * from (select date(time),round(100.0*sum(case log.status
     when '200 OK'  then 0 else 1 end)/count(log.status),3)
     as error from log group
     by date(time) order by error desc) as subq where error > 1;
                    """)
-    #To print the output 
+    # To print the output
     for result in output:
-         print (" %s: %s views" % (result[0], result[1]))
+                print (" %s: %s views" % (result[0], result[1]))
 
 print('Executing The Results please wait...\n')
 articles()
